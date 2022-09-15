602da936278c5d9a6d171cf58121be86cb6e1ffb
klibin <km.putnam2@gmail.com>
2022-09-15 15:27:59 -0600

    added custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        todo: ['Licorice'],
+        body: ['Montserrat']
+      },
+      animation: {
+        "fade-in": "fadeIn 0.5s ease-in-out"
+      },
+      keyframes: () => ({
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      })
+    },
   },
   plugins: [],
 }
