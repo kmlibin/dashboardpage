2862ee0a0220f5fadab152f629b3f6b988fcaab3
klibin <km.putnam2@gmail.com>
2022-09-16 14:26:01 -0600

    made signup, connected to firebase

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { auth, storage, database } from "../firebase/config";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useAuthContext } from "./useAuthContext";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { collection, setDoc, doc } from "firebase/firestore";
+ 
+export const useSignup = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+ 
+  const signup = async (email, password, displayName, zip, thumbnail) => {
+    setError(null);
+    setIsPending(true);
+ 
+    try {
+      // signup
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      if (!res) {
+        throw new Error("Could not complete signup");
+      }
+      // upload thumbnail
+ 
+      const storageRef = ref(
+        storage,
+        `thumbnails/uid${res.user.uid}/${thumbnail.name}`
+      );
+
+      
+      const snapShot = await uploadBytes(storageRef, thumbnail);
+      const imgUrl = await getDownloadURL(ref(storage, storageRef));
+      await updateProfile(user, { displayName, zip, photoURL: imgUrl });
+ 
+      //create a user document
+      const usersCollectionRef = doc(database, 'users', res.user.uid)
+      await setDoc(usersCollectionRef, {
+        online: true,
+        displayName,
+        zip,
+        photoURL: imgUrl,
+      });
+ 
+      dispatch({ type: "LOGIN", payload: res.user });
+ 
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (err) {
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    }
+  };
+ 
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+ 
+  return { signup, error, isPending };
+};
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+
+import { useSignup } from "../../hooks/useSignup";
+
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbError, setThumbError] = useState(null);
+  const [zip, setZip] = useState("");
+  const { signup, isLoading, error } = useSignup();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup(email, password, displayName, zip, thumbnail);
+  };
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+
+    if (!selected.type.includes("image")) {
+      setThumbError("Selected file must be an image");
+      return;
+    }
+    if (selected.size > 10000) {
+      setThumbError("Image must be less than 100kb");
+      return;
+    }
+    setThumbError(null);
+    setThumbnail(selected);
+  };
+
+  //   .auth-form {
+  //     max-width: 360px;
+  //     margin: 60px auto;
+  //     padding: 40px;
+  //     border: 1px solid #ddd;
+  //     box-shadow: 3px 3px 5px rgba(0,0,0,0.05);
+  //     background: #fff;
+  //   }
+
+  return (
+    <form
+      className="flex flex-col max-w-md my-60 mx-auto"
+      onSubmit={handleSubmit}
+    >
+      <h2>Sign Up</h2>
+      <label>
+        <p>email:</p>
+        <input
+          className="inputs"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <p>password:</p>
+        <input
+          className="inputs"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+      </label>
+      <label>
+        <p>display name:</p>
+        <input
+          className="inputs"
+          type="text"
+          value={displayName}
+          onChange={(e) => {
+            setDisplayName(e.target.value);
+          }}
+          required
+        />
+      </label>
+      <label>
+        <p>weather zipcode:</p>
+        <input
+          className="inputs"
+          value={zip}
+          type="text"
+          pattern="[0-9]*"
+          onChange={(e) => {
+            setZip(e.target.value);
+          }}
+          required
+        />
+      </label>
+      <label>
+        <p>profile thumbnail:</p>
+        <input className="inputs" type="file" onChange={handleFileChange} />
+        {thumbError && <div className="error">{thumbError}</div>}
+      </label>
+      {!isLoading && <button>Sign Up</button>}
+      {isLoading && <button disabled>Loading...</button>}
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+}
