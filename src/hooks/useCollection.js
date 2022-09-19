0b47b6963a050cba4274ac5102ed6be03f36d051
klibin <km.putnam2@gmail.com>
2022-09-19 16:42:43 -0600

    created usecollection hook

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useRef } from 'react'
+import { database } from '../firebase/config'
+
+//firebase import
+import  { collection, onSnapshot, query, where } from 'firebase/firestore'
+
+export const useCollection = (coll, _q) => {
+    const [documents, setDocuments] = useState(null)
+
+    //query
+    const q = useRef(_q).current
+
+    useEffect(() => {
+        let ref = collection(database, coll)
+        if(q) {
+            ref = query(ref, where(...q))
+        }
+        const unsub = onSnapshot(ref, (snapshot) => {
+            let results =[]
+            snapshot.docs.forEach(doc => {
+                results.push({...doc.data(), id: doc.id})
+            })
+            setDocuments(results)
+        })
+
+        return () => unsub()
+    }, [coll, q])
+
+    return { documents }
+}
