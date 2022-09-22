bd5b3dfd850df39eb0b1de361c804a65a1c1421b
klibin <km.putnam2@gmail.com>
2022-09-22 14:39:14 -0600

    adjusted size, added alternate photo if no upload

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Avatar({ src, theName }) {
+export default function Avatar({ src }) {
     return (
         <div>
-            <img className= "w-10 h-10" src={src} alt = "user photo" /> 
+            <img className= "w-14 h-14 m-2 rounded" src={src} alt = "user photo" /> 
         </div>
   )
 }
