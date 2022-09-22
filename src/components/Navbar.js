f051c8cbfbda5d7397d5fe82b7d6c8e9fd38c662
klibin <km.putnam2@gmail.com>
2022-09-22 14:39:43 -0600

    added dropdown menu, adjusted some stylings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,54 +3,63 @@ import { NavLink } from "react-router-dom";
 
 
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 import Avatar from "./Avatar";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
   const { user } = useAuthContext()
+  const {logout} = useLogout()
   
 
   return (
-    // <div className="font-body flex">
-      <div className=" font-body flex w-full ">
-        
-         <Avatar src={user.photoURL != null? user.photoURL : <i class="fa-light fas fa-user"></i>} />
+ 
+  <div className="font-body  bg-black opacity-75 border-gray-300 border-2 rounded w-1/5 flex flex-col absolute top-2 left-2">
+    <div className="w-full flex">
+       <div className="w-1/3">
+        <Avatar src={user.photoURL.includes('undefined')? 'https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_960_720.png' : user.photoURL} />
+       </div>
 
-        <div className="px-4 cursor-pointer flex items-start flex-col">
-          <p>
+        <div className="cursor-pointer flex items-center text-white w-2/3 justify-center">
+           
+          <p className="pl-2 text-2xl">
           Hey {user.displayName}
+          
             {/* change icon depending on whether menu is open or closed */}
             {!toggle ? (
               <button onClick={() => setToggle(true)}>
-                <i className="fa-solid fa-caret-down px-2"></i>
+                <i className="fa-solid fa-caret-down px-2 text-lg"></i>
               </button>
             ) : (
               <button onClick={() => setToggle(false)}>
-                <i className="fa-solid fa-caret-up px-2"></i>
+                <i className="fa-solid fa-caret-up px-2 text-lg"></i>
               </button>
             )}
           </p>
-        
+          
+</div></div>
     {/* conditionally show menu links */}
         {toggle && (
-          
-            <nav className = "flex flex-col items-start">
-              <a>
+      // "flex flex-col items-start left-12 ml-9 top-12 absolute bg-grey text-white px-3 pb-3"   
+      <div className ="w-full">
+            <nav className = "text-xs text-white list-none text-right border-t-2 p-2">
+             
+              <li className= "pt-2"><a>
                 {/* <NavLink exact to="/account"> */}
                 Account Settings
                 <i className="fa-duotone fas fa-user-gear px-2"></i>
                 {/* </NavLink> */}
-              </a>
-              <a>
+              </a></li>
+              <li className="pt-1"><button onClick = {logout}>
                 Logout
                 <i className="fa-light fas fa-right-from-bracket px-2"></i>
-              </a>
+              </button>
+              </li>
             </nav>
-         
+     </div>
         )}
-        </div>
-      </div>
-    // </div>
+        
+       </div>
   );
 }
 
