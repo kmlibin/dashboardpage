329f31131cc54736870f2cdaddd5e79852572e9b
klibin <km.putnam2@gmail.com>
2022-09-22 14:40:07 -0600

    basic stylse, added conditional for author present

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Quote({quote}) {
    
   return (
-    <div className= "animate-fade-in">
-        <p>{quote.text}</p>
-        <p>{quote.author}</p>
+    <div className= "animate-fade-in w-2/3 rounded p-5 my-10 font-medium bg-gray-300 opacity-60">
+        <p className= "text-xl">{quote.text}</p>
+        {quote.author? <p className="text-m italic">- {quote.author}</p> : <p className= "text-m italic">- Someone Wise</p>}
     </div>
   )
 }
