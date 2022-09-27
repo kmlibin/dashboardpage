1be9fc051a5fbac3b4f698af7ba9fddb3b75d7db
klibin <km.putnam2@gmail.com>
2022-09-27 15:53:13 -0600

    added error and pending states

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -5,20 +5,24 @@ import { signInWithEmailAndPassword } from 'firebase/auth'
 
 export const useLogin = () => {
     const [error, setError] = useState(null)
+    const [isPending, setIsPending] = useState(false)
     const {dispatch} = useAuthContext()
     
     const login = (email, password) => {
         setError(null)
+        setIsPending(true)
+
         signInWithEmailAndPassword(auth, email, password)
         .then((res) => {
             dispatch({type: 'LOGIN', payload: res.user})
         })
         .catch((err) => {
             setError(err.message)
+            setIsPending(false)
          })
 
 
     }
 
-    return { error, login }
+    return { error, isPending, login }
 }
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,17 +1,25 @@
-import { auth } from '../firebase/config'
-import { signOut } from 'firebase/auth'
-import { useAuthContext } from './useAuthContext'
+import { auth } from "../firebase/config";
+import { signOut } from "firebase/auth";
+import { useAuthContext } from "./useAuthContext";
+import React, {useState} from 'react'
 
 export const useLogout = () => {
-    const {dispatch} = useAuthContext()
-    const logout = () => {
-        signOut(auth)
-            .then(() => {
-                dispatch({type:'LOGOUT'})
-            })
-            .catch((err) => {
-                console.log(err.message)
-            })
-    }
-    return { logout }
-}
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = () => {
+    setError(null);
+    setIsPending(true);
+
+    signOut(auth)
+      .then(() => {
+        dispatch({ type: "LOGOUT" });
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setIsPending(false);
+      });
+  };
+  return { logout, error, isPending };
+};
