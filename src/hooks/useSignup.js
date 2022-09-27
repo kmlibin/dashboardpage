7a486d01d254f0009ef9b2418f0e6c48d63b90af
klibin <km.putnam2@gmail.com>
2022-09-27 15:54:10 -0600

    update

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -4,17 +4,17 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useAuthContext } from "./useAuthContext";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { collection, setDoc, doc } from "firebase/firestore";
- 
+
 export const useSignup = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
- 
+
   const signup = async (email, password, displayName, zip, thumbnail) => {
     setError(null);
     setIsPending(true);
- 
+
     try {
       // signup
       const res = await createUserWithEmailAndPassword(auth, email, password);
@@ -23,29 +23,33 @@ export const useSignup = () => {
         throw new Error("Could not complete signup");
       }
       // upload thumbnail
- 
+
       const storageRef = ref(
         storage,
         `thumbnails/uid${res.user.uid}/${thumbnail.name}`
       );
 
-      
       const snapShot = await uploadBytes(storageRef, thumbnail);
       const imgUrl = await getDownloadURL(ref(storage, storageRef));
-      await updateProfile(user, { displayName, uid: user.uid, zip, photoURL: imgUrl });
- 
+      await updateProfile(user, {
+        displayName,
+        uid: user.uid,
+        zip,
+        photoURL: imgUrl,
+      });
+
       //create a user document
-      const usersCollectionRef = doc(database, 'users', res.user.uid)
+      const usersCollectionRef = doc(database, "users", res.user.uid);
       await setDoc(usersCollectionRef, {
         online: true,
         displayName,
         zip,
-        uid:user.uid,
+        uid: user.uid,
         photoURL: imgUrl,
       });
- 
+
       dispatch({ type: "LOGIN", payload: res.user });
- 
+
       if (!isCancelled) {
         setIsPending(false);
         setError(null);
@@ -57,10 +61,10 @@ export const useSignup = () => {
       }
     }
   };
- 
+
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
- 
+
   return { signup, error, isPending };
 };
