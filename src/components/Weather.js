bbe7dd9bd70ceeb7f079a6336980e5889cc9e93f
klibin <km.putnam2@gmail.com>
2022-09-27 15:52:59 -0600

    added color to temp icon

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React from "react";
 
-export default function Weather({ weather}) {
-  const [loading, setIsLoading] = useState(false);
-
+export default function Weather({ weather }) {
 
   return (
     <div className="font-body w-full flex justify-end ">
       {weather && (
-        <div className="text-black flex animate-fade-in bg-grey-rgba rounded-lg mt-[10%] mx-2 shadow-equal">
+        <div className="flex animate-fade-in bg-grey-rgba rounded-lg mt-[10%] mx-2 shadow-equal">
           <img
             className="min-w-[10%]"
             src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
@@ -15,7 +13,7 @@ export default function Weather({ weather}) {
           <div className="flex flex-col justify-center pr-5">
             <p className="text-3xl">
               {weather.main.temp.toFixed(1)}
-              <i className=" pl-1 fa-regular fas fa-temperature-three-quarters"></i>
+              <i className=" pl-1 text-red-200 fa-regular fas fa-temperature-three-quarters"></i>
             </p>
             <p>{weather.weather[0].description}</p>
           </div>
