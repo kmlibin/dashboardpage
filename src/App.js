2e738e7b358ccf886bc55b1aa8ac253fcd9f8345
klibin <km.putnam2@gmail.com>
2022-09-27 15:50:42 -0600

    deleted old comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { useState } from "react";
+import React from "react";
 
 //pages and components
-import Navbar from "./components/Navbar";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
@@ -10,55 +9,18 @@ import Home from "./pages/home/Home";
 //styles
 import "./App.css";
 import { useAuthContext } from "./hooks/useAuthContext";
-import { useCollection} from"./hooks/useCollection"
-
 
 
 function App() {
-
-  const [index, setIndex] = useState(0);
+ 
   const { user, authIsReady } = useAuthContext();
-  
-  // const { documents } =  useCollection(
-  //   'users',
-  //   ['uid', '==', user.uid]
-  //   )
 
-  // //handle next button click on background image
-  // const handleRight = (e) => {
-  //   e.preventDefault();
-  //   if (index > images.length - 2) {
-  //     setIndex(0);
-  //     console.log("start over");
-  //   } else {
-  //     setIndex(index + 1);
-  //   }
-  // };
-  // //handle previous button click on background image
-  // const handleLeft = (e) => {
-  //   e.preventDefault();
-  //   if (index === 0) {
-  //     setIndex(7);
-  //     console.log("start over");
-  //   } else {
-  //     setIndex(index - 1);
-  //   }
-  // };
 
   console.log(user);
   return (
-    <div className="App">
+    <div className="App text-gray-200">
       {authIsReady && (
         <BrowserRouter>
-          {/* <div className="min-h-full min-w-full">
-        
-            <img
-              className="-z-10 absolute min-w-full min-h-full"
-              src={images[index]}
-            /> */}
-          
-            {/* <Navbar /> */}
-          {/* </div> */}
           <Routes>
             <Route path="/" element={user ? <Home /> : <Login />} />
             <Route
