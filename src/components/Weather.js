e86b81a9b992b3f1706225133a6060240427a616
klibin <km.putnam2@gmail.com>
2023-02-09 14:51:43 -0700

    adjusted margins and size

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,14 +4,14 @@ export default function Weather({ weather }) {
   return (
     <div className="font-body w-full flex justify-start sm:justify-end ">
       {weather && (
-        <div className="flex animate-fade-in bg-grey-rgba rounded-lg  sm:mt-[9%] mx-1 sm:mx-2 shadow-equal">
+        <div className="flex animate-fade-in bg-grey-rgba rounded-lg  sm:mt-[4%] mx-1 sm:mx-2 shadow-equal">
           <img
-            className="min-w-[10%]"
+            className="min-w-[8%]"
             src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
             alt="weather icon"
           />
           <div className="flex flex-col justify-center pr-5">
-            <p className="text-3xl">
+            <p className="text-2xl">
               {weather.main.temp.toFixed(1)}
               <i className=" pl-1 text-red-500 fa-regular fas fa-temperature-three-quarters opacity-75"></i>
             </p>
